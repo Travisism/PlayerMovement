3b97e399678bf90a21f4b6f7ecb4d95f4c432864
Travis Alexander <travisism@gmail.com>
2016-04-13 19:05:20 -0600

    sorta kinda crappy game

---
+++ Assets/Scripts/Behaviors/ThrowConc.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowConc : AbstractBehavior {
+
+	public GameObject concPrefab;
+
+
+	private Grenade conc;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var fire = inputState.GetButtonValue (inputButtons [0]);
+
+		if (conc == null && fire) {
+			Vector3 mousePos = Input.mousePosition;
+			Vector3 grenadePos = Camera.main.ScreenToWorldPoint (mousePos);
+			grenadePos.z = -5;
+
+			Debug.Log (grenadePos);
+
+			conc = Instantiate (concPrefab, grenadePos, Quaternion.identity) as Grenade;
+
+
+
+			Debug.Log ("Created thing");
+		}
+	}
+}
---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grenade : MonoBehaviour {
+
+
+	private Rigidbody2D body2d;
+	public float Power;
+	public float Radius;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		body2d = GetComponent<Rigidbody2D> ();
+
+		player = GameObject.FindWithTag ("Player");
+
+		Physics2D.IgnoreCollision (GetComponent<BoxCollider2D> (), player.GetComponent<CircleCollider2D> ());
+
+		StartCoroutine (ExplodeAfterDelay ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator ExplodeAfterDelay() {
+		yield return new WaitForSeconds (4);
+
+//		Vector3 mousePos = Input.mousePosition;
+//		mousePos.z = 10;
+//		Vector3 objPos1 = Camera.main.ScreenToWorldPoint(mousePos);
+		AddExplosionForce(player.GetComponent<Rigidbody2D>(), Power * 100, body2d.transform.position, Radius);
+
+		Destroy (gameObject);
+	}
+
+	public static void AddExplosionForce (Rigidbody2D body, float expForce, Vector3 expPosition, float expRadius)
+	{
+		var dir = (body.transform.position - expPosition);
+		float calc = 1 - (dir.magnitude / expRadius);
+		if (calc <= 0) {
+			calc = 0;		
+		}
+
+		body.AddForce (dir.normalized * expForce * calc);
+
+		Debug.Log ("Exploded");
+	}
+}
